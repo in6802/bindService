6556e5a3680abea84565f2d9ecfd0f538b640fdc
in6802 <in6802@gmail.com>
2018-11-16 13:11:54 +0900

    design

--- app/src/main/java/com/example/edu/bindservice/MainActivity.java
+++ app/src/main/java/com/example/edu/bindservice/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.edu.bindservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button buttonPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonPlayer = findViewById(R.id.buttonPlayer);
+        buttonPlayer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
